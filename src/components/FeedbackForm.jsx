9b7895c320c45c8a4b10e40331568f2c1661ee5c
Mark Condello <mark@dcode.com.au>
2022-01-02 14:36:54 +1100

    included RatingSelect conponent

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [message, setMessage] = useState(null)
   const [btnDisabled, setBtnDisabled] = useState(true)
 
@@ -25,7 +27,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating)=> setRating(rating)}/>
         <div className="input-group">
           <input 
             onChange={handleTextChange} 
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+function RatingSelect({select}) {
+  const [selected, setSelected] = useState(10)
+  const handleChange = (ev) => {
+    // console.log('Reached handleChange, val:', parseInt(ev.currentTarget.value))
+    setSelected(parseInt(ev.currentTarget.value))
+    select(parseInt(ev.currentTarget.value)) // function passed through as a prop
+  }
+  return (
+    <ul className="rating">
+      <li><input value="1" type="radio" name="rating" id="num1" checked={selected === 1} onChange={handleChange} /><label htmlFor="num1">1</label></li>
+      <li><input value="2" type="radio" name="rating" id="num2" checked={selected === 2} onChange={handleChange} /><label htmlFor="num2">2</label></li>
+      <li><input value="3" type="radio" name="rating" id="num3" checked={selected === 3} onChange={handleChange} /><label htmlFor="num3">3</label></li>
+      <li><input value="4" type="radio" name="rating" id="num4" checked={selected === 4} onChange={handleChange} /><label htmlFor="num4">4</label></li>
+      <li><input value="5" type="radio" name="rating" id="num5" checked={selected === 5} onChange={handleChange} /><label htmlFor="num5">5</label></li>
+      <li><input value="6" type="radio" name="rating" id="num6" checked={selected === 6} onChange={handleChange} /><label htmlFor="num6">6</label></li>
+      <li><input value="7" type="radio" name="rating" id="num7" checked={selected === 7} onChange={handleChange} /><label htmlFor="num7">7</label></li>
+      <li><input value="8" type="radio" name="rating" id="num8" checked={selected === 8} onChange={handleChange} /><label htmlFor="num8">8</label></li>
+      <li><input value="9" type="radio" name="rating" id="num9" checked={selected === 9} onChange={handleChange} /><label htmlFor="num9">9</label></li>
+      <li><input value="10" type="radio" name="rating" id="num10" checked={selected === 10} onChange={handleChange} /><label htmlFor="num10">10</label></li>
+    </ul>
+  )
+}
+
+export default RatingSelect
