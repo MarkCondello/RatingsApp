51de6b7d76c775f14fc030d5ce5c6a8842329c83
Mark Condello <mark@dcode.com.au>
2022-01-02 16:44:49 +1100

    added routes with v5.2.1 router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { v4 as uuidv4} from 'uuid'
 import { useState } from 'react'
+import { BrowserRouter as Router , Route } from 'react-router-dom'
+
+import About from './pages/About'
 import FeedbackData from './data/Feedbackdata'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -20,17 +23,20 @@ function App() {
     setFeedback([newFeedback, ...feedback] )
   })
   return (
-  <>
+  <Router>
     <Header text='Ratings App' />
     <div className="container">
-      <FeedbackForm handleAdd={addFeedback}/>
-      <FeedbackStats feedback={feedback} />
-      <FeedbackList 
-        feedback={feedback}
-        handleDelete={deleteFeedback}
-      />
+      <Route exact path='/'> 
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList 
+          feedback={feedback}
+          handleDelete={deleteFeedback}
+          />
+      </Route>
+      <Route path='/about' component={About} />
       </div>
-   </>
+   </Router>
   )
 }
 
