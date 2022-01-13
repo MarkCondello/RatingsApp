66a28da04ea4f53f8961dd1273dfa93d62de6129
Mark Condello <mark@dcode.com.au>
2022-01-13 21:51:10 +1100

    -persisted content changes to json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,20 +1,39 @@
-import { v4 as uuidv4} from 'uuid'
-import { createContext, useState } from 'react'
-import FeedbackData from '../data/Feedbackdata'
+// import { v4 as uuidv4} from 'uuid'
+import { createContext, useState, useEffect } from 'react'
+// import FeedbackData from '../data/Feedbackdata'
+import axios from 'axios'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState(FeedbackData)
-  const deleteFeedback = ((id) => {
-    if(window.confirm('Are you sure you want to delete this item?')) {
-      setFeedback(feedback.filter(item => item.id !== id))
-    }
-  })
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   }) 
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    axios.get('http://localhost:5000/feedback?_sort=id&_order=desc')
+      .then(resp => {
+        setIsLoading(false)
+        setFeedback(resp.data)
+      })
+  }
+  const deleteFeedback = ((id) => {
+    if(window.confirm('Are you sure you want to delete this item?')) {
+      axios.delete(`http://localhost:5000/feedback/${id}`)
+        .then(resp => {
+          // console.log("reached deleteFeedback,", {resp})
+          setFeedback(feedback.filter(item => item.id !== id))
+        })
+    }
+  })
+
   //Set Item to be updated
   const editFeedback = (item => {
     // console.log('editFeedback: ', {item})
@@ -26,21 +45,29 @@ export const FeedbackProvider = ({children}) => {
   //Update feedbackEdit item which is set
   const updateFeedback = (id, updatedItem) => {
     //console.log({id, updatedItem})
-    setFeedback(feedback.map(item => {
-      return item.id === id ? { ...item, ...updatedItem } : item
+    axios.put(`http://localhost:5000/feedback/${id}`, updatedItem)
+      .then(resp => {
+        // console.log('updateFeedback method reached,', {resp})
+        setFeedback(feedback.map(item => {
+          return item.id === id ? { ...item, ...updatedItem } : item
+        }))
       })
-    )
   }
   //Set new Feedback item
   const addFeedback = (newFeedback => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback] )
+    axios.post('http://localhost:5000/feedback', newFeedback)
+      .then(resp => {
+        // console.log('addFeedback method reached,', {resp})
+        setFeedback([newFeedback, ...feedback] )
+      })
+    // newFeedback.id = uuidv4()
   })
   return <FeedbackContext.Provider 
     value={{
       feedback,
       deleteFeedback,
       feedbackEdit,
+      isLoading,
       addFeedback,
       editFeedback,
       updateFeedback
